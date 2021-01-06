5c285d2954b4e826dc86a6cb605cef89775eb4e2
STS-NIVEDHITHA R <STS-NIVEDHITHA R@STS-L300>
2021-01-06 13:40:04 +0530

    Comments added

--- sdk-dotnet/APIClientTool/Utilities/OAuthGenerator.cs
+++ sdk-dotnet/APIClientTool/Utilities/OAuthGenerator.cs
@@ -17,24 +17,30 @@ namespace APIClientTool.Utilities
         /// <param name="client">The client.</param>
         public static void GenerateJWSAndGetAccessToken(HttpClient client)
         {
-            #region Get Credentials from TaxBandits Dev Console
+            #region Get Credentials from TaxBandits Dev Console (https://sandbox.taxbandits.com/ (Settings ==> API Credentials))
 
+            //Fetch the 3 keys from web.config file
             string userToken = Utility.GetAppSettings("UserToken");
             string clientId = Utility.GetAppSettings("ClientId");
             string clientSecret = Utility.GetAppSettings("ClientSecret");
 
             #endregion
 
+            //Encode the JWS with clientSecret to create a signature
             var clientSecretSymmetricKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(clientSecret));
             var signingCredentials = new SigningCredentials(clientSecretSymmetricKey, SecurityAlgorithms.HmacSha256);
+
+            //Create the required set of claims with appropriate values
             var claims = new[] {
                                     new Claim(JwtRegisteredClaimNames.Iss, clientId),
                                     new Claim(JwtRegisteredClaimNames.Sub, clientId),
                                     new Claim(JwtRegisteredClaimNames.Aud, userToken),
                                     new Claim(JwtRegisteredClaimNames.Iat, ConvertToUnixEpochTimestamp(DateTime.Now).ToString()),
                                };
-
+            //Setting the 5 minutes expiry to the JWS
             var token = new JwtSecurityToken(null, null, claims, expires: DateTime.Now.AddMinutes(5), signingCredentials: signingCredentials);
+
+            //WriteToken method constructs & returns the JWS
             var jws = new JwtSecurityTokenHandler().WriteToken(token);
 
             client.DefaultRequestHeaders.Clear();
@@ -52,6 +58,7 @@ namespace APIClientTool.Utilities
         /// <returns></returns>
         public static double ConvertToUnixEpochTimestamp(DateTime date)
         {
+            //The difference of current time & the origin (in seconds) as per Unix epoch standard is calculated
             DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
             TimeSpan diff = date.ToUniversalTime() - origin;
             return Math.Floor(diff.TotalSeconds);
@@ -67,6 +74,8 @@ namespace APIClientTool.Utilities
         public static void ConstructHeadersWithAccessToken(HttpClient client, string accessToken)
         {
             client.DefaultRequestHeaders.Clear();
+
+            //Add the JWT along with Bearer keyword to the Authorization header
             var jwtAccessToken = $"Bearer {accessToken}";
             client.DefaultRequestHeaders.Add("Accept", "application/json");
             client.DefaultRequestHeaders.Add("Authorization", jwtAccessToken);
